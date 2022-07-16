73843c014dc2dd3ebe740e985edff1ab8b8ab14a
Vladimir Ryabchenko <79854322262@yandex.ru>
2022-07-16 21:00:38 +0300

    Render Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from './data';
 import './App.css';
 
 function App() {
@@ -46,65 +47,19 @@ function App() {
         <main className="main">
             <div className="content">
                 <ul className="products">
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="/images/d1.jpg" alt="product" />
-                            <div className="product-name">
-                                <a href="product.html">Slim shirts</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Start (10 Reviews)</div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="/images/d1.jpg" alt="product" />
-                            <div className="product-name">
-                                <a href="product.html">Slim shirts</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Start (10 Reviews)</div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="/images/d1.jpg" alt="product" />
-                            <div className="product-name">
-                                <a href="product.html">Slim shirts</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Start (10 Reviews)</div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="/images/d1.jpg" alt="product" />
-                            <div className="product-name">
-                                <a href="product.html">Slim shirts</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Start (10 Reviews)</div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="/images/d1.jpg" alt="product" />
-                            <div className="product-name">
-                                <a href="product.html">Slim shirts</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Start (10 Reviews)</div>
-                        </div>
-                    </li>
+                    {data.products.map(product => (
+                      <li>
+                          <div className="product">
+                              <img className="product-image" src={product.image} alt="product" />
+                              <div className="product-name">
+                                  <a href="product.html">{product.name}</a>
+                              </div>
+                              <div className="product-brand">{product.brand}</div>
+                              <div className="product-price">${product.price}</div>
+                              <div className="product-rating">{product.ratins} Stars ({product.numReviews} Reviews)</div>
+                          </div>
+                      </li>
+                    ))}
                 </ul>
             </div>
         </main>
---
+++ frontend/src/data.js
@@ -0,0 +1,51 @@
+const data = {
+    products: [
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+        },
+        {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 5,
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+        },
+        {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 5,
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+        },
+    ]
+};
+
+export default data;
