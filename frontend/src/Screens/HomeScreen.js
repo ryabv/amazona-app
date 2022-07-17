56d40e76fcd438553670a5272ead583806c615bf
Vladimir Ryabchenko <79854322262@yandex.ru>
2022-07-17 16:05:20 +0300

    Fetch Server Data

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,17 +1,28 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
-import data from '../data';
+import axios from 'axios';
 
 const HomeScreen = (props) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios.get('/api/products');
+            setProducts(data);
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <ul className="products">
-            {data.products.map((product) => (
+            {products.map((product) => (
             <li key={product._id}>
                 <div className="product">
                     <Link to={`/product/${product._id}`}>
                         <img className="product-image" src={product.image} alt="product" />
                     </Link>
-                    
+
                     <div className="product-name">
                         <Link to={`/product/${product._id}`}>{product.name}</Link>
                     </div>
